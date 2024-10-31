193d98ba27ed47e7a41627fb4a1c240ec5c6c5e8
ThzEverton <evertonthomazdelimasilva070@gmail.com>
2024-10-31 20:47:41 -0300

    Pipe feito

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,13 +1,18 @@
+import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,TitleCasePipe,CurrencyPipe,DatePipe],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
+
+nomeFantasia ='operative tecnologia';
+faturamento = 22222.222;
+data = '2023-07-12';
 
 }
---
+++ src/app/reduz-nome.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reduzNome',
+  standalone: true
+})
+export class ReduzNomePipe implements PipeTransform {
+
+  transform(texto:string): string {
+    if(!texto){
+      return"";
+
+    }
+    const nomes=texto.split(' ');
+    const primeiroNome = nomes[0];
+    const ultimoNome = nomes[nomes.length - 4];
+    return '${primeiroNome} ${ultimoNome}';
+  }
+
+}
