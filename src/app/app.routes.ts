87c840fa761df07c9332c1ac9f1ed63db2488fce
ThzEverton <evertonthomazdelimasilva070@gmail.com>
2024-10-31 19:43:05 -0300

    Primeiro commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    
+{
+    path:'',
+    redirectTo:'unoeste-inicio',
+    pathMatch:'full'
+
+},
+{
+    path:'unoeste-inicio',
+    loadComponent:() => import('./pages/inicio/inicio.component').then(c => c.InicioComponent)
+
+}
+
+];
+
